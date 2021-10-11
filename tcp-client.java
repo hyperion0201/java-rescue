5098df7715d373a0efffe5c57ab2e404cb4cb02c
Hieu Hoang <megamind.ft@gmail.com>
2021-10-12 01:37:23 +0700

    Create tcp-client.java

---
+++ tcp-client.java
@@ -0,0 +1,47 @@
+// Java Program to implement ServerSocket class
+// Client - side
+
+// Importing required libraries
+import java.io.*;
+import java.net.*;
+
+// Main class
+public class MyClient {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+
+		// Try block to check if exception occurs
+		try {
+
+			// Creating Socket class object and
+			// initializing Socket
+			Socket soc = new Socket("localhost", 6666);
+
+			DataOutputStream d = new DataOutputStream(
+				soc.getOutputStream());
+
+			// Message to be displayed
+			d.writeUTF("Hello GFG Readers!");
+
+			// Flushing out internal buffers,
+			// optimizing for better performance
+			d.flush();
+
+			// Closing the connections
+
+			// Closing DataOutputStream
+			d.close();
+			// Closing socket
+			soc.close();
+		}
+
+		// Catch block to handle exceptions
+		catch (Exception e) {
+
+			// Print the exception on the console
+			System.out.println(e);
+		}
+	}
+}
